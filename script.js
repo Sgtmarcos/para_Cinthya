ad99e4edaddc5ee45c57fe5f1ffd76bc40253d5d
Marcos Vinicius <79850252+Sgtmarcos@users.noreply.github.com>
2024-11-13 11:47:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['POSITIVAÇÃO DE CLIENTES'], 'text',['papayawhip','ghostwhite','lightblue']);
+ consoleText(['Para Mayara'], 'text',['papayawhip','ghostwhite','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
